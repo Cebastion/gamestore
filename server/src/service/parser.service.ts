2c1afb461b9c5b464baf8a570b64284de337ab4b
Cebastion <99135718+Cebastion@users.noreply.github.com>
2023-12-06 22:11:34 +0300

    new commit

--- server/src/service/parser.service.ts
+++ server/src/service/parser.service.ts
@@ -12,8 +12,6 @@ export default class ParserService {
   private sourceElement = ('https://images.gog-statics.com/45a284386e693f1576b96d98a0023a7905d3956c6f9aa913d3fe5d09a5994bee_product_tile_extended_432x243.webp')
   private list_genre = []
 
-  PostGames() { }
-
   GetGames() {
     for (let page = 0; page < max_count_page; page++) {
       axios.get(this.URL + `?page=${page}`).then(html => {
