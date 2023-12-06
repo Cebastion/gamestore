cbac5c4c2459b52783ecdcfa7d83d81d58ddb050
Cebastion <99135718+Cebastion@users.noreply.github.com>
2023-12-06 12:32:26 +0300

    Update parser.service.ts

--- server/src/service/parser.service.ts
+++ server/src/service/parser.service.ts
@@ -12,12 +12,12 @@ export default class ParserService {
       if (res.status === 200) {
         const $ = cheerio.load(res.data)
         const getTitle = $('body > div.main-content > div > div.products-trending > div.listing-items.listing-slider > div:nth-child(1) > div > div.text > div > span').text()
-        const directory = '/json'
+        const directory = '../json'
         if (!fs.existsSync(directory)) {
           fs.mkdirSync(directory)
         }
         const games = JSON.stringify(getTitle)
-        fs.writeFileSync(`./json/games.json`, games)
+        fs.writeFileSync(`../json/games.json`, games)
         console.log(getTitle)
         return getTitle
       }
