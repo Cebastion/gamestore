c6d59dd0a84741a41f44235f3385a76fbb715f8e
Cebastion <99135718+Cebastion@users.noreply.github.com>
2023-12-17 13:15:53 +0300

    new comit

--- client/src/service/Game.service.ts
+++ client/src/service/Game.service.ts
@@ -1,9 +1,17 @@
-import { IGames } from '@/interface/Game.interface'
+import { IGames, IGame } from '@/interface/Game.interface'
 import axios from 'axios'
 
 export const GameService = {
   async GetGameList(page?: number, genres: string[] | string = '', priceRange?: number[]){
     const {data} = await axios.get<IGames>(`http://localhost:5500/?page=${page}&genres=${genres}&priceRange=${priceRange}`)
     return data
+  },
+  AddGameBascket(SelectGame: IGame) {
+    const NewArray: IGames = [];
+    return NewArray.games?.push(SelectGame)
+  },
+  DeleteGameBascket(SelectGame: IGame, array: IGames){
+    const NewArray = array.games?.filter(game => game.game !== SelectGame)
+    return NewArray
   }
 }
