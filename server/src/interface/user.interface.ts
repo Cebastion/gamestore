69d4e9f957fff02e91152c6d3022c5331de377a2
Cebastion <kdimon2006@gimal.com>
2023-12-18 15:38:31 +0200

    new commit

--- server/src/interface/user.interface.ts
+++ server/src/interface/user.interface.ts
@@ -1,10 +1,8 @@
 import { IGames } from './game.interface'
 
 export interface IUser {
-  user: {
-    Name: string;
-    Email: string;
-    Password: string;
-    games: IGames
-  }
+  Name: string
+  Email: string
+  Password: string
+  games?: IGames
 }
---
+++ server/src/service/user.service.ts
@@ -0,0 +1,46 @@
+import path from 'path'
+import fs from 'fs'
+import { IGames } from '../interface/game.interface'
+import { IUser } from '../interface/user.interface'
+
+export class UserService{
+  private name: string
+  private password: string
+  private email: string
+  private games: IGames
+
+  private WriteUser(User: IUser) {
+    const directory = path.resolve(__dirname, '/users')
+    if(!fs.existsSync(directory)){
+      fs.mkdirSync(directory)
+    }
+    const UserJson = JSON.stringify(User)
+    fs.writeFileSync(directory + '/users.json', UserJson)
+  }
+
+  constructor(name: string, password: string, email:string, games: IGames){
+    this.name = name
+    this.password = password
+    this.email = email
+    this.games = games
+  }
+
+  LogIn(User: IUser){
+    const isAuthenticated = User.Email === this.email && User.Password === this.email
+    if(isAuthenticated){
+
+    } else {
+      return {message: ""}
+    }
+  }
+
+
+  SignIn(User:IUser){
+    User = {
+      Name: this.name,
+      Email: this.email,
+      Password: this.password,
+    }
+    this.WriteUser(User)
+  }
+}
