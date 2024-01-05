4b308184bf4d6aae4cbd7d70e16f592e09a760e4
Cebastion <kdimon2006@gimal.com>
2024-01-05 12:27:43 +0200

    new commit

--- client/src/service/Game.service.ts
+++ client/src/service/Game.service.ts
@@ -4,17 +4,17 @@ import axios from 'axios'
 
 export const GameService = {
   async GetGameList(page?: number, genres: string[] | string = '', priceRange?: number[]) {
-    const { data } = await axios.get<IGames>(`http://localhost:5500/?page=${page}&genres=${genres}&priceRange=${priceRange}`)
+    const { data } = await axios.get<IGames>(`https://gamestore-azure.vercel.app/?page=${page}&genres=${genres}&priceRange=${priceRange}`)
     return data
   },
 
   async LogIn(email: string, password: string) {
-    const { data } = await axios.post<IUser>('http://localhost:5500/login', { email, password })
+    const { data } = await axios.post<IUser>('https://gamestore-azure.vercel.app/login', { email, password })
     return { data }
   },
 
   async SignIn(name: string, email: string, password: string) {
-    const { data } = await axios.post<IUser>('http://localhost:5500/signin', { name, email, password })
+    const { data } = await axios.post<IUser>('https://gamestore-azure.vercel.app/signin', { name, email, password })
     return { data }
   }
 }
